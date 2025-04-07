f32a32f14eab342e16d6c1813685a0b9fbbdec5e
Vladimir Burdukov <vladimir_burdukov@epam.com>
2025-04-07 12:04:19 +0300

    Automatic update Mon Apr  7 12:04:19 EEST 2025

--- omf/init.fish
+++ omf/init.fish
@@ -1,7 +1,7 @@
 rvm default
 
 alias ls='gls --color=auto'
-# alias git="gitext"
+alias git="gitext"
 alias s='subl'
 alias killsim="launchctl remove com.apple.CoreSimulator.CoreSimulatorService"
 alias sm="smerge ."
@@ -19,6 +19,7 @@ else
     set -Ux EDITOR 'subl -w'
 end
 
+set -xg fish_cursor_default underscore
 set -xg theme_color_scheme terminal
 set -xg theme_nerd_fonts yes
 set -xg theme_date_format "+%F %r"
@@ -29,7 +30,11 @@ set -xg LC_ALL en_US.UTF-8
 set -xg LANG en_US.UTF-8
 
 fish_add_path -a "$HOME/.cargo/bin"
-fish_add_path -a "$HOME/.bin"
+fish_add_path -a "$HOME/.local/bin"
+fish_add_path -a "$HOME/.usd/bin"
 fish_add_path -a "/opt/homebrew/sbin"
 
+set -xg PYTHONPATH "/Users/Vladimir_Burdukov/.usd/lib/python"
 set -xg RUST_SRC_PATH "$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+
+set -xg SSH_AUTH_SOCK (launchctl getenv SSH_AUTH_SOCK)
